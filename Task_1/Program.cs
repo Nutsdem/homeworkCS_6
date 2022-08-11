4467345344e33a88278ea7530d96a7cdeb4a0b3d
nutsdem <nechaeff91@gmail.com>
2022-08-11 16:26:55 +0500

    Task_2

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+void getNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int getComp(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+getNumbers(m);
+
+Console.WriteLine($"Чисел больше 0: {getComp(array)} ");
